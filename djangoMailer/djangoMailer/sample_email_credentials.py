31a3467bbd931893a489a235709f565d224026f6
pulkitpahwa <pulkitpahwa11@gmail.com>
2016-04-26 08:46:57 +0530

    sample_email_credentials file added

---
+++ djangoMailer/djangoMailer/sample_email_credentials.py
@@ -0,0 +1,8 @@
+EMAIL_HOST = '' #your email host
+EMAIL_HOST_USER = '' #your email host user
+EMAIL_HOST_PASSWORD = '' #your password
+EMAIL_PORT = 587 #change it to the port of your provide
+EMAIL_USE_TLS = True 
+SERVER_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
